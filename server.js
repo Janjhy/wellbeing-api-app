b19f4aa92b554fcd6df7e37ad54ab47d8ef10d4b
Janjhy <jani.hyrkas@gmail.com>
2021-04-21 17:02:47 +0300

    Changed the server to use dotenv and hosted db

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 let express = require("express");
 let bodyparser = require("body-parser");
 let mongoose = require("mongoose");
+require('dotenv').config();
 
 let app = express();
 let port = 8066;
-let config = require("./config/dev.js");
 let routes = require("./routes");
+const uri = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASS + "@cluster0.ybpd1.mongodb.net/" + process.env.DB_NAME + "?retryWrites=true&w=majority";
 
-mongoose.connect(config.mongodatabase.uri + "/" + config.mongodatabase.db_name, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(bodyparser.urlencoded({extended: true}));
 routes.apiRouter(app, mongoose);
@@ -16,8 +17,6 @@ app.use((_, res) => {
    res.status(404);
    res.json({error: 'Invalid URL'});
 });
-
-
 
 app.listen(port);
 
